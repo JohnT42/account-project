14fbe35de9000d68ceb12acb3f425065e22a81d8
JohnT <j.tran24@hotmail.com>
2018-11-13 13:19:40 +0000

    Second Commit

--- src/main/java/com/qa/simple/account_application/App.java
+++ src/main/java/com/qa/simple/account_application/App.java
@@ -1,13 +1,18 @@
 package com.qa.simple.account_application;
 
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
     public static void main( String[] args )
     {
         System.out.println( "Hello world to the standard out" );
+        Service user=new Service();
+        user.addAccount("The", "Shafeeq");
+        user.addAccount("Orion", "Pax");
+        user.addAccount("Stan", "Lee");
+        user.getAccountDetails(3);
     }
 }
