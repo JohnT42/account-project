4eff09ad6b8a25ccf37e7e5f4093b0e09b3e6460
JohnT <j.tran24@hotmail.com>
2018-11-13 16:26:54 +0000

    Fifth commit

---
+++ src/test/java/com/qa/simple/account_application/TddTest.java
@@ -0,0 +1,44 @@
+package com.qa.simple.account_application;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TddTest {
+@Test
+public void test1() {
+	Service user=new Service();
+	user.addAccount("Peter", "Tran","2");
+	user.addAccount("Peter", "Tran","3");
+	user.addAccount("Peter", "Tran","4");
+	assertEquals(0, user.countName("John"));
+	}
+
+@Test
+public void test2() {
+	Service user=new Service();
+	user.addAccount("Peter", "Tran","2");
+	user.addAccount("Peter", "Tran","3");
+	user.addAccount("John", "Tran","4");
+	assertEquals(0, user.countName("John"));
+	}
+
+@Test
+public void test3() {
+	Service user=new Service();
+	user.addAccount("Peter", "Tran","2");
+	user.addAccount("John", "Tran","3");
+	user.addAccount("John", "Tran","4");
+	assertEquals(0, user.countName("John"));
+	}
+
+@Test
+public void test4() {
+	Service user=new Service();
+	user.addAccount("John", "Tran","2");
+	user.addAccount("John", "Tran","3");
+	user.addAccount("John", "Tran","4");
+	assertEquals(0, user.countName("John"));
+	}
+}
+
