db2b1174b3d9de914b2088cfeaf1a513e3b1867d
JohnT <j.tran24@hotmail.com>
2018-11-13 13:21:20 +0000

    Third commit

---
+++ src/main/java/com/qa/simple/account_application/Person.java
@@ -0,0 +1,30 @@
+package com.qa.simple.account_application;
+
+public class Person {
+	String firstName;
+	String lastName;
+	int accountNumber;
+ public Person(String firstName, String lastName) {
+			this.firstName=firstName;
+			this.lastName=lastName;
+			}
+		public  String getFirstname() {
+			return firstName;
+			}
+		
+		public  String getLastname() {
+			return lastName;
+			}
+		
+		/*public int getaccountNumber() {
+			return accountNumber;
+			}
+		
+		public  String setFirstname() {
+			return firstName;
+			}
+		
+		public  String setLastname() {
+			return lastName;
+			}*/
+}
